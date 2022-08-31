8cf21f153ddc2aac9a7a711f64c462d2636077be
JCooler <jon.cooler@yahoo.com>
2022-08-31 12:29:03 -0400

    Dark mode toggle changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,22 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
 
- 
+$('#main').toggleClass(localStorage.toggled);
+
+function darkLight() {
+  /*DARK CLASS*/
+  if (localStorage.toggled != 'dark-mode') {
+    $('#main, p').toggleClass('dark-mode', true);
+    localStorage.toggled = "dark-mode";
+     
+  } else {
+    $('#main, p').toggleClass('dark-mode', false);
+    localStorage.toggled = "";
+  }
+}
+
+/*Add 'checked' property to input if background == dark*/
+if ($('main').hasClass('dark-mode')) {
+   $( '#checkBox' ).prop( "checked", true )
+} else {
+  $( '#checkBox' ).prop( "checked", false )
+}
+
