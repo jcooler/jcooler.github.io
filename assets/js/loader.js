b122c7ff6eaeedd48076159559a2df8a0e8ee654
jcooler <jonathan.r.cooler@gmail.com>
2023-01-16 17:07:53 -0500

    loader.js

---
+++ assets/js/loader.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+
+loader.classList.add("loader-hidden");
+
+
+
+loader.addEventListener("transitioned", () => {
+
+    document.body.removeChild("loader");
+});
+
+})
